f751c54fae15e56d8a39d2095a4ba8d58876e658
gervic08 <germanvicentin@gmail.com>
2022-06-13 10:39:24 -0300

    Stock lookup aund users style views added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_07_175824) do
+ActiveRecord::Schema.define(version: 2022_06_09_194156) do
+
+  create_table "stocks", force: :cascade do |t|
+    t.string "name"
+    t.string "ticker"
+    t.decimal "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
