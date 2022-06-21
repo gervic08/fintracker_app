e01975f9c56b4e8a11f39722154e735bffe90acb
gervic08 <germanvicentin@gmail.com>
2022-06-21 16:14:37 -0300

    Update user profile added to navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,4 +21,9 @@ class User < ApplicationRecord
   def can_track_stock?(ticker)
     under_max_tracked_stocks? && stock_already_tracked?(ticker)
   end
+
+  def full_name
+    return "#{first_name} #{last_name}" if first_name || last_name
+    "Anonymous"
+  end
 end
