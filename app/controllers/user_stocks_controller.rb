d496fbb085e58bf2da00cbf7955a497ce1a4ac65
gervic08 <germanvicentin@gmail.com>
2022-06-21 13:59:00 -0300

    Remove stock button added to my_portfolio

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -1,21 +1,24 @@
 # frozen_string_literal: true
 
 class UserStocksController < ApplicationController
-  before_action :set_stock
-
+  
   def create
     stock ||= Stock.lookup(params[:ticker])
     stock.save! unless stock.persisted?
-    @user_stock = UserStock.create(user: current_user, stock: stock)
-    if @user_stock.save!
+    user_stock = UserStock.create(user: current_user, stock: stock)
+    if user_stock.save!
       flash[:notice] = "Stock #{stock.name} successfully added to portfolio"
       redirect_to my_portfolio_path
     end  
   end
 
-  private
-
-  def set_stock
-    stock = Stock.find_by(ticker: params[:ticker])
+  def destroy
+    stock = Stock.find_by(id: params[:id])
+    user_stock = UserStock.where(user_id: current_user, stock_id: stock.id).first
+    if user_stock.destroy
+      flash[:notice] = "Stock #{stock.name} is not tracked anymore" 
+      redirect_to my_portfolio_path
+    end
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :user_stocks, only: :create
+  resources :user_stocks, only: %i[create destroy]
   root 'welcome#index'
   devise_for :users
   get 'my_portfolio', to: 'users#my_portfolio'
