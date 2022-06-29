2cd0ee2eff162d1616f062afc42b602c861fce0c
gervic08 <germanvicentin@gmail.com>
2022-06-29 16:38:50 -0300

    Fixed welcome page after signout

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 class WelcomeController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
+
   def index
+    unless current_user
+      redirect_to '/users/sign_in'
+    end
   end
 end
